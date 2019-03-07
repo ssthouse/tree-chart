15a10d5be0554255ce89568b03d79ee0dd6714bd
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-07 03:47:00 +0000

    Deploy ssthouse/organization-chart to github.com/ssthouse/organization-chart.git:gh-pages

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
   entry: './src/main.js',
   output: {
-    path: path.resolve(__dirname, './dist'),
+    path: path.resolve(__dirname, './docs'),
     filename: 'bundle.js'
   },
   module: {
