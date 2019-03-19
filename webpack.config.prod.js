f96cf586135d19661719b3f917d6e914d63e5d2e
ssthouse <ssthouse@163.com>
2019-03-19 16:33:03 +0800

    Fix prod script missing base configuration

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,43 +1,13 @@
 const merge = require('webpack-merge')
 const webpack = require('webpack')
 
-module.exports = merge({
+module.exports = merge(require('./webpack.config.base'), {
+  mode: 'production',
   devtool: '#source-map',
   output: {
     publicPath: './'
   },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: '"production"'
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true,
-      compress: {
-        warnings: false
-      }
-    }),
-    new webpack.LoaderOptionsPlugin({
-      minimize: true
-    })
-  ]
+  optimization: {
+    minimize: true
+  }
 })
-// module.exports.devtool = '#source-map'
-// // http://vue-loader.vuejs.org/en/workflow/production.html
-// module.exports.plugins = (module.exports.plugins || []).concat([
-//   new webpack.DefinePlugin({
-//     'process.env': {
-//       NODE_ENV: '"production"'
-//     }
-//   }),
-//   new webpack.optimize.UglifyJsPlugin({
-//     sourceMap: true,
-//     compress: {
-//       warnings: false
-//     }
-//   }),
-//   new webpack.LoaderOptionsPlugin({
-//     minimize: true
-//   })
-// ])
