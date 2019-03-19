ce77fffc60c085ebc83660085f8c2134e3d38431
ssthouse <ssthouse@163.com>
2019-03-19 16:03:14 +0800

    Extract webpack configuration into dev && prod mode

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -2,13 +2,10 @@ const path = require('path')
 const { VueLoaderPlugin } = require('vue-loader')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-const isDevMode = process.env.NODE_ENV !== 'production'
-
 module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './'),
-    publicPath: isDevMode ? '/' : './',
     filename: 'bundle.js'
   },
   module: {
@@ -49,25 +46,4 @@ module.exports = {
       vue: 'vue/dist/vue.js'
     }
   }
-}
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports.devtool = '#source-map'
-  // http://vue-loader.vuejs.org/en/workflow/production.html
-  module.exports.plugins = (module.exports.plugins || []).concat([
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: '"production"'
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true,
-      compress: {
-        warnings: false
-      }
-    }),
-    new webpack.LoaderOptionsPlugin({
-      minimize: true
-    })
-  ])
 }
---
+++ webpack.config.prod.js
@@ -0,0 +1,42 @@
+const merge = require('webpack-merge')
+
+module.exports = merge({
+  devtool: '#source-map',
+  output: {
+    publicPath: './'
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
+    })
+  ]
+})
+// module.exports.devtool = '#source-map'
+// // http://vue-loader.vuejs.org/en/workflow/production.html
+// module.exports.plugins = (module.exports.plugins || []).concat([
+//   new webpack.DefinePlugin({
+//     'process.env': {
+//       NODE_ENV: '"production"'
+//     }
+//   }),
+//   new webpack.optimize.UglifyJsPlugin({
+//     sourceMap: true,
+//     compress: {
+//       warnings: false
+//     }
+//   }),
+//   new webpack.LoaderOptionsPlugin({
+//     minimize: true
+//   })
+// ])
