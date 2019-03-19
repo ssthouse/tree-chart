97dfe951f1eb5648c8b0991abf4250fde6ccf449
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-19 05:40:20 +0000

    Deploy ssthouse/organization-chart to github.com/ssthouse/organization-chart.git:gh-pages

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -48,6 +48,7 @@ const devWebpackConfig = merge(baseWebpackConfig, {
     new webpack.DefinePlugin({
       'process.env': require('../config/dev.env')
     }),
+    new webpack.LoaderOptionsPlugin({ options: {} }),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NamedModulesPlugin(), // HMR shows correct file names in console on update.
     new webpack.NoEmitOnErrorsPlugin(),
--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './'),
-    publicPath: './',
+    publicPath: '/',
     filename: 'bundle.js'
   },
   module: {
