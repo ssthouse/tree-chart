2a90a36ccbf383e7ff62aff3a949209aabdf6ef6
timshen <timshen@tencent.com>
2020-05-10 15:52:19 +0800

    feat: 将输出目录改为dist目录

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -5,7 +5,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
   entry: './src/main.js',
   output: {
-    path: path.resolve(__dirname, './'),
+    path: path.resolve(__dirname, './dist'),
     filename: 'bundle.js'
   },
   module: {
