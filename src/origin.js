4eb77bf5bcee943cf152fc9ffcab36867cceaf16
ssthouse <ssthouse@163.com>
2020-06-21 17:09:33 +0800

    chore: remove unused files

--- src/origin.js
+++ src/origin.js
@@ -2,26 +2,24 @@ var d3 = require('d3')
 
 var base = d3.select('#container')
 
-var chart = base.append('canvas')
-  .attr('width', 400)
-  .attr('height', 300)
+var chart = base.append('canvas').attr('width', 400).attr('height', 300)
 var context = chart.node().getContext('2d')
 
 var dataContainer = base.append('custom')
 
-function drawCustom (data) {
-  var scale = d3.scaleLinear()
-    .range([10, 390])
-    .domain(d3.extent(data))
+function drawCustom(data) {
+  var scale = d3.scaleLinear().range([10, 390]).domain(d3.extent(data))
 
-  var dataBinding = dataContainer.selectAll('custom.rect')
-    .data(data, function (d) { return d })
+  var dataBinding = dataContainer
+    .selectAll('custom.rect')
+    .data(data, function (d) {
+      return d
+    })
+
+  dataBinding.attr('size', 15).attr('fillStyle', 'green')
 
   dataBinding
-    .attr('size', 15)
-    .attr('fillStyle', 'green')
-
-  dataBinding.enter()
+    .enter()
     .append('custom')
     .classed('rect', true)
     .attr('x', scale)
@@ -29,14 +27,12 @@ function drawCustom (data) {
     .attr('size', 8)
     .attr('fillStyle', 'red')
 
-  dataBinding.exit()
-    .attr('size', 5)
-    .attr('fillStyle', 'lightgrey')
+  dataBinding.exit().attr('size', 5).attr('fillStyle', 'lightgrey')
 
   drawCanvas()
 }
 
-function drawCanvas () {
+function drawCanvas() {
   // clear canvas
   context.fillStyle = '#fff'
   context.rect(0, 0, chart.attr('width'), chart.attr('height'))
@@ -48,13 +44,18 @@ function drawCanvas () {
 
     context.beginPath()
     context.fillStyle = node.attr('fillStyle')
-    context.rect(node.attr('x'), node.attr('y'), node.attr('size'), node.attr('size'))
+    context.rect(
+      node.attr('x'),
+      node.attr('y'),
+      node.attr('size'),
+      node.attr('size')
+    )
     context.fill()
     context.closePath()
   })
 }
 
-function run () {
+function run() {
   drawCustom([1, 2, 13, 20, 23])
 }
 
