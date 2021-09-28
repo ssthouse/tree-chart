98caf215526f1c23a8206c644b40aa2f0159b766
ssthouse <ssthouse@163.com>
2021-09-28 15:36:56 +0800

    feat: change local development default route to svgTree

--- src/demo/router/index.ts
+++ src/demo/router/index.ts
@@ -10,7 +10,7 @@ export default new Router({
   routes: [
     {
       path: '/',
-      redirect: Cons.CANVAS_TREE
+      redirect: Cons.SVG_TREE
     },
     {
       path: '/' + Cons.CANVAS_TREE,
