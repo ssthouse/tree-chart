cc007fd2a1c920db52093c859b16e1d4bb3edee9
ssthouse <timshen@tencent.com>
2021-12-09 20:07:03 +0800

    feat: add vue-tree-chart dev demo page

---
+++ packages/vue-tree-chart/src/demo/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <button @click="onChangeDataset">change dataset</button>
+
+    <vue-tree
+      style="width: 800px; height: 600px; border: 1px solid gray"
+      :dataset="sampleData"
+      :config="treeConfig"
+    />
+  </div>
+</template>
+
+<script>
+import VueTree from "../vue-tree/VueTree.vue";
+
+export default {
+  components: { VueTree },
+  data() {
+    return {
+      showTree: false,
+      sampleData: {
+        value: "1",
+        children: [
+          { value: "2", children: [{ value: "4" }, { value: "5" }] },
+          { value: "3" },
+        ],
+      },
+      treeConfig: { nodeWidth: 120, nodeHeight: 80, levelHeight: 200 },
+    };
+  },
+  methods: {
+    onChangeDataset() {
+      this.sampleData = {
+        value: "100",
+        children: [
+          { value: "200" },
+          { value: "300", children: [{ value: "400" }, { value: "500" }] },
+        ],
+      };
+    },
+    onClick() {
+      this.showTree = true;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
