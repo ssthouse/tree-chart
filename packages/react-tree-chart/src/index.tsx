44572b04d9fd7881adbf2b36b241ce56328fe145
ssthouse <timshen@tencent.com>
2021-12-16 15:36:02 +0800

    feat: implement react-tree basic function

--- packages/react-tree-chart/src/index.tsx
+++ packages/react-tree-chart/src/index.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './demo/App';
+import TreeChart from './tree-chart';
+
+const sampleData = {
+  value: "1",
+  children: [
+    { value: "2", children: [{ value: "4" }, { value: "5" }] },
+    { value: "3" },
+  ],
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <TreeChart dataset={sampleData} style={{
+      width: '600px',
+      height: '600px',
+      border: '1px solid black'
+    }} />
   </React.StrictMode>,
   document.getElementById('root')
 );
--- packages/react-tree-chart/src/tree-chart.tsx
+++ packages/react-tree-chart/src/tree-chart.tsx
@@ -1,7 +1,132 @@
-import React from 'react';
+import React, { forwardRef, useEffect, useImperativeHandle, useRef, useState } from 'react';
+import TreeChartCore, { DEFAULT_LEVEL_HEIGHT, DEFAULT_NODE_HEIGHT, DEFAULT_NODE_WIDTH, Direction, TreeLinkStyle } from '@ssthouse/tree-chart-core';
+import './tree-chart.scss';
 
-const TreeChart = () => {
-  return <div >React tree chart</div>
+const formatDimension = (dimension: number | string) => {
+  if (typeof dimension === "number") return `${dimension}px`;
+  if (dimension.indexOf("px") !== -1) {
+    return dimension;
+  } else {
+    return `${dimension}px`;
+  }
+};
+
+interface TreeChartConfig {
+  nodeWidth: number;
+  nodeHeight: number;
+  levelHeight: number;
 }
+
+interface TreeChartProps {
+  config?: TreeChartConfig;
+  linkStyle?: TreeLinkStyle;
+  direction?: Direction;
+  collapseEnabled?: boolean;
+  dataset: Object | Object[];
+  style?: React.CSSProperties;
+}
+
+const DEFAULT_CONFIG = {
+  nodeWidth: DEFAULT_NODE_WIDTH,
+  nodeHeight: DEFAULT_NODE_HEIGHT,
+  levelHeight: DEFAULT_LEVEL_HEIGHT,
+};
+
+const TreeChart = forwardRef((props: TreeChartProps, ref) => {
+  const {
+    direction = Direction.VERTICAL,
+    config = DEFAULT_CONFIG,
+    dataset,
+    collapseEnabled = true,
+    style = {},
+  } = props;
+  const [treeChartCore, setTreeChartCore] = useState<TreeChartCore>();
+  const [initialTransformStyle, setInitialTransformStyle] = useState({})
+  const [nodeDataList, setNodeDataList] = useState<any[]>([]);
+
+  // refs
+  const containerRef = useRef<HTMLDivElement>(null);
+  const domContainerRef = useRef<HTMLDivElement>(null);
+  const svgRef = useRef<SVGSVGElement>(null);
+
+  // init tree chart core with refs
+  useEffect(() => {
+    console.log('~~~~~~~~~~~~~~')
+    const treeChartCore = new TreeChartCore({
+      svgElement: svgRef.current as SVGElement,
+      domElement: domContainerRef.current as HTMLDivElement,
+      treeContainer: containerRef.current as HTMLDivElement,
+      dataset,
+      collapseEnabled,
+    });
+    treeChartCore.init();
+    const nodeDataList = treeChartCore.getNodeDataList();
+    const initialTransformStyle = treeChartCore.getInitialTransformStyle();
+
+    setTreeChartCore(treeChartCore);
+    setInitialTransformStyle(initialTransformStyle)
+    setNodeDataList(nodeDataList);
+  }, [])
+
+  const onClickNode = (index: number) => {
+    if (!treeChartCore) return;
+    treeChartCore.onClickNode(index);
+    setNodeDataList(treeChartCore.getNodeDataList());
+  }
+
+  useImperativeHandle(ref, () => {
+    return {
+      zoomIn() {
+        treeChartCore?.zoomIn();
+      },
+      zoomOut() {
+        treeChartCore?.zoomOut();
+      },
+      restoreScale() {
+        treeChartCore?.setScale(1);
+      },
+    }
+  })
+  return <div className="tree-container"
+    ref={containerRef} style={style}>
+    <svg className="svg vue-tree" ref={svgRef} style={initialTransformStyle}></svg>
+    <div
+      className="dom-container"
+      ref={domContainerRef}
+      style={initialTransformStyle}
+    >
+      {/* <transition-group name="tree-node-item" tag="div"> */}
+
+      {
+        nodeDataList.map((node, index) => {
+          return <div
+            className="node-slot"
+            onClick={() => onClickNode(index)}
+            key={node.data._key}
+            style={{
+              left: formatDimension(
+                direction === Direction.VERTICAL ? node.x : node.y
+              ),
+              top: formatDimension(
+                direction === Direction.VERTICAL ? node.y : node.x
+              ),
+              width: formatDimension(config.nodeWidth),
+              height: formatDimension(config.nodeHeight),
+            }}
+          >
+            {/* <slot
+              name="node"
+              v-bind:node="node.data"
+              v-bind:collapsed="node.data._collapsed"
+            > */}
+            <span>{node.data.value}</span>
+            {/* </slot> */}
+          </div>
+        })
+      }
+      {/* </transition - group > */}
+    </div >
+  </div >
+});
 
 export default TreeChart;
